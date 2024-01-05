baf89e61addf1792191f1701b5d758ea2e0d72c2
tang <119taeyoung@naver.com>
2024-01-05 14:18:39 +0900

    declaration file: how to import js module to ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi"
-  }
-}
+import { exit, init } from "myPackage";
+
+init({ url: "true "})
+exit(1)
+
+localStorage.clear()
